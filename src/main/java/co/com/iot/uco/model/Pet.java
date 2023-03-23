1092c1a4e3ca0f9c565f79a3650bb0d197c1b2cd
Jorge Lima <ldjorge1988@gmail.com>
2023-03-23 17:02:40 -0500

    se adiciona adicion de campo id obligatorio para un Entity

--- src/main/java/co/com/iot/uco/model/Pet.java
+++ src/main/java/co/com/iot/uco/model/Pet.java
@@ -1,7 +1,6 @@
 package co.com.iot.uco.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -16,7 +15,10 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @Table(name = "pet")
 public class Pet {
-
-
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
 }
